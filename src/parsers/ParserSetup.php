d6a2609f4d1d8a47d647cdfdf4387fc2e1f6f8f7
AMN009 <aymennorhalloussi@gmail.com>
2022-01-27 00:33:14 +0100

    after merging

--- src/parsers/ParserSetup.php
+++ src/parsers/ParserSetup.php
@@ -3,7 +3,7 @@
     // $xml_doc_location = __DIR__."/../xml/cinema.xml";
     $doc = new DomDocument();
     $xml_doc_location="../xml/cinema.xml";
-    $doc->load($xml_doc_location);
+    $doc->load(__DIR__."/../xml/cinema.xml");
     $root = $doc->documentElement;
 
 ?>
