88f5f8a1c337c4bc869758ad639fd2427cd0a810
fahd bouacherine <okyby2018@gmail.com>
2022-01-26 17:01:14 +0100

    last up

--- src/views/admin/lists/reservations/scripts/script.php
+++ src/views/admin/lists/reservations/scripts/script.php
@@ -1,12 +1,12 @@
 <?php 
   $xp = new XSLTProcessor();
   $xsl = new DomDocument;
-  $xsl->load('Reservation.xsl');
+  $xsl->load('../stylesheets/style.xsl');
   // importer la feuille de style XSL dans le processus XSLT
    $xp->importStylesheet($xsl);
 
    $xml_doc = new DomDocument;
-   $xml_doc->load('../../../../xml/cinema.xml');
+   $xml_doc->load('../../../../../xml/cinema.xml');
    print $xp->transformToXML($xml_doc)
    
 ?>
