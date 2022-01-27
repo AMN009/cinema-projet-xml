15f275ea6eefdf6a337f4ab21d1eaee879f4ec14
fahd bouacherine <okyby2018@gmail.com>
2022-01-27 14:29:44 +0100

    prepare modal

---
+++ src/views/admin/lists/salles/SalleFormUpdate.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Davur - Restaurant Food Order Bootstrap Admin Dashboard</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../style/images/favicon.png">
+    	<link href="../../../style/vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+    <link href="../../../style/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+            
+                                    <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#exampleModalCenter">Modal centered</button>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModalCenter">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-danger light" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+            
+
+
+            <script src="../../../style/vendor/global/global.min.js"></script>
+	<script src="../../../style/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="../../../style/vendor/chart.js/Chart.bundle.min.js"></script>
+    <script src="../../../style/js/custom.min.js"></script>
+	<script src="../../../style/js/deznav-init.js"></script>
+	<!-- Apex Chart -->
+	<script src="../../../style/vendor/apexchart/apexchart.js"></script>
+</body>
+</html>
